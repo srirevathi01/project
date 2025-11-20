120bf51659b7ecb815ee4f36cd5212667dbe7025
srirevathi01 <srirevathi1810@gmail.com>
2025-11-20 18:26:00 +0530

    Modified app.py and index.html

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {"message": "Hello from API"}
+    return {"message": "Hello from Backend"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
